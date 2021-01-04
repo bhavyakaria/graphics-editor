54855f61340f28e993ba0b4a2e4f4e900d62d46a
bhavyakaria <bhavyakaria@yahoo.in>
2021-01-04 23:52:05 +0530

    build: version 0.0.2

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/graphics-editor"));
+app.use(express.static(__dirname + "/dist/PainApplication"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/graphics-editor/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/PainApplication/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
